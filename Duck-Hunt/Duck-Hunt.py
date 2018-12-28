5030f38e5ca33bea7764f01b7a04630aafc74d65
Paul\0 <paul-marie.bettinelli@epitech.eu>
2018-12-28 10:32:07 +0000

    Add Event's logic
    Tthis version is abble to open a pygame's window, and display wich sort of event was caught.

--- Duck-Hunt/Duck-Hunt.py
+++ Duck-Hunt/Duck-Hunt.py
@@ -3,4 +3,20 @@
 
 import pygame
 
-print("Hello World")
+pygame.init()
+
+window = pygame.display.set_mode((800,600))
+pygame.display.set_caption("Mon super jeu video")
+clock = pygame.time.Clock()
+leave = False
+
+while not leave:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            leave = True
+        print(event)
+    pygame.display.update()
+    clock.tick(60)
+
+pygame.quit()
+quit()
